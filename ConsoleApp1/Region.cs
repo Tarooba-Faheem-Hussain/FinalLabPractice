5437ea7c040bc42c5a5e310afad5a08e819d22e5
Tarooba Faheem <tarooba.faheem.hussain@gmail.com>
2021-11-29 03:47:37 +0500

    first commit for final lab practice

---
+++ ConsoleApp1/Region.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Region
+    {
+        public TownMap TownMap
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int desc
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TownMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class TownMap : IMAP
+    {
+        public int name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JumpToLocation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
